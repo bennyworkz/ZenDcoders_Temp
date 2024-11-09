72f447d563f867e6f272bbfbd120917e3a98299c
sattisatyanarayanareddy <ssnarayanareddy1126@gmail.com>
2024-11-09 09:53:59 +0530

    developer commit

---
+++ src/Components/Home/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../Themes/Styles.css";
+import ZenDcodersLogoDark from '../Assets/ZenDcoders_Logo_Dark.png';
+
+
+const NavBar = () => {
+  return (
+    <div>
+        <nav className="navbar">
+                <img src={ZenDcodersLogoDark} alt="Zendcoders" className="logo1" />
+                </nav>
+      
+    </div>
+  )
+}
+
+export default NavBar
